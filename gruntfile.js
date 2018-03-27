63197874119521685aa45d9e4b2cdc6b1e7ada6a
Arbass <13ar.bass@gmail.com>
2018-03-27 17:48:41 +0300

    Подключает Watch

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,29 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-less');
-    
-    grunt.initConfig({
-        less: {
-            style: {
-                files: {
-                    "css/style.css": "less/style.less"
-                }
-            }
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "css/style.css": "less/style.less" // destination file and source file
         }
-    });
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
 };
