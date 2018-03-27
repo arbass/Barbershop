8c399acaffe728694663fcfcb1e6bfef50a10ea9
Arbass <13ar.bass@gmail.com>
2018-03-27 14:35:15 +0300

    +

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function(frunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     
     grunt.initConfig({
-        
+        less: {
+            style: {
+                files: {
+                    "css/style.css": "less/style.less"
+                }
+            }
+        }
     });
 };
